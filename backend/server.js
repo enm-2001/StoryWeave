b9d76efc76266446493fd91589d72fabbdd3a481
Ekta Mer <ektamer4@gmail.com>
2023-07-07 15:06:49 +0530

    first

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
 const express = require("express")
+const bodyParser = require("body-parser");
 const cors = require("cors")
+//const path = require
 
 const app = express();
 app.use(cors())
+
+
+app.get('/',(req, res) => {
+    res.send("Home")
+})
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000....");
+})
