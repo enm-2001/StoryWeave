6ab4b00232148f54c315f92be87648be4c986b46
Ekta Mer <ektamer4@gmail.com>
2023-07-12 17:14:32 +0530

    startstory

--- frontend/src/components/StartStory.vue
+++ frontend/src/components/StartStory.vue
@@ -1,22 +1,64 @@
 <template>
-    <div class="main-bg">
-        <div class="card">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <input type="text" class="card-title">
-    <textarea class="card-text"></textarea>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+  <div class="main-bg">
+    <div class="card">
+      <div class="card-header">Start your Story</div>
+      <div class="card-body">
+        <input
+          type="text"
+          placeholder="Title of Story"
+          class="card-title"
+          v-model="story.title"
+          required
+        /><br />
+        <span v-if="titleNotExist" style="color: red;">Please enter the title of your story..</span>
+        <textarea
+          class="card-text"
+          placeholder="Start you story...."
+          v-model="story.description"
+          required
+        ></textarea
+        ><br />
+        <button @click="postStory()">Post</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'StartStory'
-    }
+import router from "@/router/routes";
+import axios from "axios";
+
+export default {
+  name: "StartStory",
+  data() {
+    return {
+      story: {
+        title: "",
+        description: "",
+      },
+      titleNotExist: false
+    };
+  },
+  methods: {
+    postStory() {
+      if (this.story.title != "") {
+        const user_id = JSON.parse(localStorage.getItem("user")).user_id;
+        axios
+          .post("http://localhost:5000/api/story/create", {
+            story: this.story,
+            user_id,
+          })
+          .then((res) => {
+            console.log("Story posted..", res);
+            router.push("/dashboard");
+          })
+          .catch((err) => console.log(err));
+      } else {
+        this.titleNotExist = true;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -37,18 +79,48 @@
   width: 200vh;
 }
 
-.card{
-    height: 500px;
-    width: 600px;
-    border-radius: 5px;
+.card {
+  height: 400px;
+  width: 600px;
+  border-radius: 5px;
   margin: 20px auto;
-  background-color: F9F5F6;
+  /* background-color: rgba(96, 150, 180, 0.8); */
+  background-color: rgba(255, 255, 255, 0.8);
+  /* background-color: F9F5F6; */
   border-radius: 5px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
   font-family: Avenir, Helvetica;
-    color: #2c3e50;
-     border-radius: 15px;
-   /*  margin:12px; */
-     padding:0px;
+  color: #2c3e50;
+  border-radius: 15px;
+  /*  margin:12px; */
+  padding: 0px;
+}
+
+.card-header{
+  color: #223e4d;
+}
+.card-title {
+  width: 100%;
+}
+
+.card-text {
+  width: 100%;
+  height: 50%;
+  overflow-y: scroll;
+}
+
+button {
+  background-color: #6096b4;
+  color: white;
+  border: none;
+  padding: 12px 12px;
+  font-size: 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-align: center;
+  border-radius: 10px;
+  display: flex;
+  margin: auto;
+  width: 20%;
 }
 </style>
