e70c7f7a4c3bf82ef082d912fed6d2315b47cdba
Hritika <hagarwal@argusoft.com~>
2023-07-22 10:36:18 +0530

    dashboard

--- frontend/src/components/DashboardBottom.vue
+++ frontend/src/components/DashboardBottom.vue
@@ -1,63 +1,53 @@
 <template>
-  <div class="mainbg">
-    <div class="stories">
-      <div class="col-6">
-        <div class="col-text">Read Story</div>
-      </div>
-      <div class="col-6">
+<div class="mainbg">
+<div class="stories">
+    <div class="col-6">
+        <div class="col-text"> Read Story</div>
+    </div>
+    <div class="col-6">
         <div class="col-text">Contribute Story</div>
-      </div>
     </div>
-    <div class="rwcomp" style="z-index:5">
-      <ReadComponent class="rcomp" />
-      <WriteComponent class="wcomp" />
-    </div>
-  </div>
-</template>
-
-<script>
-import ReadComponent from "./ReadComponent.vue";
-import WriteComponent from "./WriteComponent.vue";
+</div>
+<div class="rwcomp">
+    <ReadComponent class="rcomp" />
+    <WriteComponent class="wcomp"/>
+</div>
+</div>
+</template><script>
+import ReadComponent from './ReadComponent.vue';
+import WriteComponent from './WriteComponent.vue';
 export default {
-  name: "DashboardBottom",
-  data: () => {
-    return {};
-  },
-  components: {
-    ReadComponent,
-    WriteComponent,
-  },
-};
+    name: "DashboardBottom",
+    components: {
+        ReadComponent,
+        WriteComponent,
+    },
+    };
 </script>
 <style scoped>
-
 .mainbg{
-    z-index: -1;
          background-image: url("../assets/back3.avif");
       background-repeat: no-repeat, repeat;
-       background-position: center; 
+       background-position: center;
   background-size: cover;
-  position: relative;
 }
-
-
 .stories {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  padding-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding-top: 20px;
 }
 .col-text {
-  font-family: Belanosima;
-  color: #537188;
-  font-size: 25px;
-  font-weight: bolder;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.col-6 {
-  padding: 0px;
-  margin: 0px;
+    font-family: Belanosima;
+    color: #537188
+;
+    font-size: 25px;
+    font-weight: bolder;
+    display: flex;
+    align-items: center;
+    justify-content: center;}
+    .col-6 {
+    padding: 0px;
+    margin: 0px;
 }
 </style>
